d253f268d383bc4c4ed56a013c29ada2865a4d0e
Gasaron <145772461+Gasaron@users.noreply.github.com>
2024-02-22 10:51:15 -0300

    V2.0

---
+++ Aula-03/Aula-03/logica.php
@@ -0,0 +1,22 @@
+<?php
+class Banco{
+    private static $dbName = 'db_brazuna';
+    private static $dbHost = 'localhost';
+    private static $dbUser = 'root';
+    private static $dbPass = '';
+    private static $cont = null;
+
+    public static function conectar(){
+        if(null == self::$cont){
+            try{
+                self::$cont = new PDO("mysql:host" . self::$dbHost . ";" . "dbname=" . self::$dbName, self::$dbUser, self::$dbPass);
+            }catch(PDOException $ex){
+                die($ex->getMessage());
+            }
+        }
+        return self::$cont;
+    }
+    public static function desconectar(){
+        self::$cont = null;
+    }
+}
